50246b46ecd905e048e18cb8358ad5c28ce48e01
Myeshathecreator <amreen.08kazi@gmail.com>
2026-05-11 18:49:30 +0400

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,17 @@
+a=input("Enter value of a: ")
+b=input("Enter value of b: ")
+c=input("Enter value of c: ")
+
+temp=a
+a=c
+c=b
+b=temp
+
+print("The value of 'a' after swapping is:" , a)
+print("The value of 'b' after swapping is:" , b)
+print("The value of 'c' after swapping is:" , c)
+
+
+
+
+
