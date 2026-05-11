398e1fbcbf24c86ef9c7b6f3213944368a1cf83d
Myeshathecreator <amreen.08kazi@gmail.com>
2026-05-11 18:20:31 +0400

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter a dividend: "))
+n2= int(input("Enter a divisor: "))
+
+if n1%n2==0:
+    print(n1, "is divisible by" ,n2)
+else:
+    print(n1, "is not divisible by" ,n2)
---
+++ Mean.py
@@ -0,0 +1,19 @@
+# Wrong sum
+wavg=38
+wn= 36
+cn=56
+tn=40
+wsum= wavg*tn
+print("The wrong sum of 40 numbers: ", wsum)
+
+#Correct calculation
+
+sum= wsum-((wn)- (cn))
+print("Correct sum is: ", sum)
+
+#Correct avg
+
+cavg=sum/tn
+print("Correct mean is:", cavg)
+
+
